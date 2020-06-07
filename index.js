852340d3a99b5847d898c9b60d8c1bcedb178838
PC LINK <pclink@PCs-MacBook-Pro.local>
2020-06-07 20:41:49 +0100

    Completed Project

---
+++ index.js
@@ -0,0 +1,8 @@
+var superHeroes = require("superheroes");
+var superVillains = require("supervillains");
+
+var mySuperHeroesName = superHeroes.random();
+var myVillainName = superVillains.random();
+
+console.log(mySuperHeroesName);
+console.log(myVillainName);
